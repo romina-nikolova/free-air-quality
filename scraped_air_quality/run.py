87a0dff5bc8c17e952445c1d2058e8b1f0a855da
LAPTOP-80D0FLTN\romin <romina.nikolova1@gmail.com>
2021-12-06 10:53:04 +0100

    add runtime file

--- scraped_air_quality/run.py
+++ scraped_air_quality/run.py
@@ -12,12 +12,13 @@ yesterday = datetime.datetime.strftime(today - datetime.timedelta(days=1), forma
 config = configparser.ConfigParser()
 config.read(os.path.join(os.getenv('HOME'), 'config.ini'))
 
-print('Starting scraping...')
-#os.system("scrapy runspider get_scraped_data.py -o file.csv -t csv")
-print('Scraping completed.')
-print('Processing scraped data...')
-df = process_scraped_data.process_scraped_data('file')
-yesterday_df = df[df['measure_date'] == yesterday]
-print('Processing completed')
-print('Starting load to bitdotio')
-load_scraped_data.load_pandas_to_bitdotio(df)
+if __name__ == "__main__":
+    print('Starting scraping...')
+    os.system("scrapy runspider get_scraped_data.py -o file.csv -t csv")
+    print('Scraping completed.')
+    print('Processing scraped data...')
+    df = process_scraped_data.process_scraped_data('file')
+    yesterday_df = df[df['measure_date'] == yesterday]
+    print('Processing completed')
+    print('Starting load to bitdotio')
+    load_scraped_data.load_pandas_to_bitdotio(df)
